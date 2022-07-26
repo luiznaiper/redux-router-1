3ed4a8abb4404d3fb7c77515e037d9d549f5b2c4
luiznaiper <luiz_naiper@hotmail.com>
2022-07-26 07:51:06 -0500

    :construction: Add login component

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../containers/Home";
+import Login from "../containers/Login";
 
 const App = () => (
   <Router>
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
     </Routes>
   </Router>
 );
