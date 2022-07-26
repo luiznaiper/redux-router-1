28260ef4f232e6a6bdd3fee8482c06734412bb31
luiznaiper <luiz_naiper@hotmail.com>
2022-07-26 07:59:08 -0500

    :construction: Add register section

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../containers/Home";
 import Login from "../containers/Login";
+import Register from "../containers/Register";
 
 const App = () => (
   <Router>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
     </Routes>
   </Router>
 );
