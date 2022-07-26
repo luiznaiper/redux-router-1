141180bbda75edb998a5eeca5d632467708fba13
luiznaiper <luiz_naiper@hotmail.com>
2022-07-26 07:37:09 -0500

    :construction: Make the route app works

---
+++ src/routes/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../containers/Home";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
