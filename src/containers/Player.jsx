35d34bea90a50c703476d39dfa5197abe1eda8c3
luiznaiper <luiz_naiper@hotmail.com>
2022-07-28 09:22:32 -0500

    :construction: Add player video functions

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useNavigate, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import { getVideoSource } from '../actions';
 import '../assets/styles/components/Player.scss';
 
-const Player = () => {
-  return (
+const Player = (props) => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const hasPlaying = Object.keys(props.playing).length > 0;
+  useEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
     <div className="Player">
       <video controls autoPlay>
-        <source src="" type="video/mp4" />
+        <source src={props.playing.source} type="video/mp4" />
       </video>
-      <div className="Player-black">
-        <button type="button"></button>
+      <div className="Player-back">
+        <button type="button" onClick={() => navigate(-1)}>
+          Go back
+        </button>
       </div>
     </div>
+  ) : (
+    <>
+      <h2>That video does not exist</h2>
+      <Link to="/">Back to home</Link>
+    </>
   );
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
