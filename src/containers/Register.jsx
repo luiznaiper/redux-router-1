3cc75852cb0a6601e2b082a58e9f5717b6236ca5
luiznaiper <luiz_naiper@hotmail.com>
2022-07-28 07:56:25 -0500

    :construction: Add register validation with useState

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,19 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../assets/styles/components/Register.scss";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/components/Register.scss';
 
 const Register = () => {
+  const [values, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(values);
+  };
+
   return (
     <section className="register">
       <section className="register__container">
         <h2>Regístrate</h2>
-        <form className="register__container--form">
-          <input className="input" type="text" placeholder="Nombre" />
-          <input className="input" type="text" placeholder="Correo" />
-          <input className="input" type="password" placeholder="Contraseña" />
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input
+            name="name"
+            className="input"
+            type="text"
+            placeholder="Nombre"
+            onChange={handleInput}
+          />
+          <input
+            name="email"
+            className="input"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
           <button className="button">Registrarme</button>
         </form>
-        <Link to="/login">Iniciar sesión</Link>
+        <Link to="/login">
+          Already have an account? <strong>Log in</strong>
+        </Link>
       </section>
     </section>
   );
