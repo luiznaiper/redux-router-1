7ecd8562d07fa630f4e1577c3daf9ed8effa09fd
luiznaiper <luiz_naiper@hotmail.com>
2022-07-28 08:16:39 -0500

    :construction: Add player reproductor

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "../components/Layout";
-import Home from "../containers/Home";
-import Login from "../containers/Login";
-import Register from "../containers/Register";
-import NotFound from "../containers/NotFound";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from '../components/Layout';
+import Home from '../containers/Home';
+import Login from '../containers/Login';
+import Register from '../containers/Register';
+import Player from '../containers/Player';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
   <Router>
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
+        <Route exact path="/player/:id" element={<Player />} />
         <Route exact path="*" element={<NotFound />} />
       </Routes>
     </Layout>
