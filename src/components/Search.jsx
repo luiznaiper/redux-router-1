e9e0b392a511da17710661549622b56966f0580b
luiznaiper <luiz_naiper@hotmail.com>
2022-07-29 07:43:17 -0500

    :construction: Search component working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
-import classNames from 'classnames';
+import React, { useState, useMemo } from 'react';
+import { connect } from 'react-redux';
+import CarouselItem from './CarouselItem';
+
 import '../assets/styles/components/Search.scss';
 
-const Search = ({ isHome }) => {
-  const inputStyle = classNames('input', {
-    isHome,
-  });
+const Search = (props) => {
+  const [result, setResult] = useState({});
+  const [search, setSearch] = useState({ search: '' });
+  const handleSearch = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+  useMemo(() => {
+    setResult(
+      props.trends.concat(props.originals).filter((item) => {
+        return item.title.toLowerCase().includes(search.search.toLowerCase());
+      })
+    );
+  }, [search]);
   return (
     <section className="main">
       <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-      <input type="text" className={inputStyle} placeholder="Buscar..." />
+      <input
+        name="search"
+        onChange={handleSearch}
+        type="text"
+        className="input"
+        placeholder="Buscar..."
+      />
+      <div className="main-search">
+        <div className="carousel__container">
+          {!search.search
+            ? ''
+            : result.map((item) => {
+                return <CarouselItem key={item.id} {...item} />;
+              })}
+        </div>
+      </div>
     </section>
   );
 };
-
-export default Search;
+const mapStateToProps = (state) => {
+  return {
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+export default connect(mapStateToProps, null)(Search);
